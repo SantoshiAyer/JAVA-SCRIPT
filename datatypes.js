1cd829395125c6ea3f84716116f9a7accc15254e
santoshi ayer <>
2024-04-10 15:39:02 +0545

    this is fourth file

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+// string
+const name = 'ram';
+const name1 = "hari";
+
+// Number
+const number1 = 3; // Integer
+const number2 = 3.433; // Floating-point
+const number3 = 3e5; // Exponential notation (3 * 10^5)
+
+//undefined
+let a; 
+
+let a = null; //null
+
+const value = Symbol('hello');//symbol
+
+const student = {}; // Empty object
+
+
+
+
---
+++ fourth.js
@@ -0,0 +1,17 @@
+const accountId =12223
+let accountEmail="santoshiayer12@gmail.com"
+var accountPassword="1234"
+accountCity="Kanchanpur"
+// accountId=2//not allowed
+console.log(accountId);
+
+
+accountEmail="sanu12@gmail.com"
+accountPassword="5678"
+accountCity="mahendranagar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+// prefer not use var becausde of issue in block scope and functional scope
+
