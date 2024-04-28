5bfb64cbfe89b041499881e099c1c2d8e01f70fe
SANTOSHI AYER <108856386+SantoshiAyer@users.noreply.github.com>
2024-04-28 10:16:36 +0545

    Add files via upload

---
+++ 01-fuction.js
@@ -0,0 +1,10 @@
+// function addTwoNumber(number1,number2){
+// console.log(number1+number2);
+// }
+// const result =addTwoNumbers(3,5)
+// console.log(result);
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  let result=myFunction(5,3);
+  console.log(result);
