7762a20e255a0b5e6b1f14d626c0022e7d25dac5
SANTOSHI AYER <108856386+SantoshiAyer@users.noreply.github.com>
2024-04-30 09:36:28 +0545

    Add files via upload

--- 01-fuction.js
+++ 01-fuction.js
@@ -3,8 +3,22 @@
 // }
 // const result =addTwoNumbers(3,5)
 // console.log(result);
-function myFunction(p1, p2) {
-    return p1 * p2;
-  }
-  let result=myFunction(5,3);
-  console.log(result);
+// function myFunction(p1, p2) {
+//     return p1 * p2;
+//   }
+//   let result=myFunction(5,3);
+//   console.log(result);
+
+
+function calculateCartPrice(...num1){
+  return num1;
+}
+// console.log(calculateCartPrice(200,400,500))
+const user={
+  username:"santu"
+  price:199
+}
+function handleObject(anyobject){
+  console.log('username is $ {anyobject.username'and price is $ {anyobject.price});
+}
+handleObject(user)
