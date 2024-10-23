eacd67fd3d71da8e599fedc231e5f0e82603bb02
santoshi ayer <santoshiayer12@gmail.com>
2024-10-23 19:26:49 +0545

    commited

---
+++ SCOPE.js
@@ -0,0 +1,19 @@
+
+function addOne(num) {
+    return num + 1;
+}
+console.log(addOne(2)); // Call the function to log the result
+
+const addTwo = function(num) {
+    return num + 2;
+};
+console.log(addTwo(2)); // Call the function to log the result
+
+
+
+
+
+
+
+
+
--- closure.js
+++ closure.js
@@ -1,19 +1,16 @@
 // fuction hello1(){
-//    let message="good morning"
-// console.log("hello1:" +message)}
-
-
-
+//let message="good morning"
+//console.log("hello1:" +message)}
 
 function outerFunction() {
-   var outerVariable = 'I am from the outer function';
-   
-   function innerFunction() {
-     console.log(outerVariable); 
-   }
-   
-   return innerFunction;
- }
- 
- var innerFunc = outerFunction();
- innerFunc()
+var outerVariable = 'I am from the outer function';
+
+function innerFunction() {
+console.log(outerVariable); 
+}
+
+return innerFunction;
+}
+
+var innerFunc = outerFunction();
+innerFunc()
