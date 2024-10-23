e63aea81973c6cfa228e8f2604b4f9e0aba30aae
santoshi ayer <santoshiayer12@gmail.com>
2024-10-23 21:25:44 +0545

    this and arrow function

---
+++ arrow.js
@@ -0,0 +1,22 @@
+const user={
+    username:"sanu",
+    price:999,
+
+    welcomeMessage:function(){
+        console.log('${this.username},welcome to website');
+    }
+
+}
+user.welcomeMessage()
+
+
+
+// this le current content lai refer grx
+
+ const addTwo(num1,num2) =>{
+    return num1+num2
+ }
+ console.log(addTwo(3,4))
+
+ //if use curly bracket return keyword written nd if not dont written return keyword
+ 
