5a479064b4b7592df58f536138391a104534345e
santoshi ayer <santoshiayer12@gmail.com>
2024-10-28 08:52:53 +0545

    commit

--- EVENT/practice.js
+++ EVENT/practice.js
@@ -1,4 +1,13 @@
 //create a toggle button that changes the screen to dark mode when clicked and light mode when clicked again.
 let modeBtn=document.querySelector('#mode');
-
+let currMode="light"
+modeBtn.addEventListener('click',()=>{
+    if(currMode==="light"){
+     currMode="dark"; 
+     document.querySelector("body").style.backgroundColor="black";
+    }else{
+        currMode="light";
+    }
+    console.log(currMode);
+})
 
