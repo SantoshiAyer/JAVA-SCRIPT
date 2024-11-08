af5b886dbf5879b0d15e050e0b3750ac90eae9c5
santoshi ayer <santoshiayer12@gmail.com>
2024-11-08 07:07:58 +0545

    commit

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,9 +1,9 @@
 let myPromise = new Promise((resolve, reject) => {
     // Simulate an asynchronous operation
     let success = true; // Change this to false to test rejection
-  
+
     if (success) {
-      resolve('Operation successful!');
+    resolve('Operation successful!');
     } else {
     reject('Operation failed!');
     }
@@ -17,5 +17,27 @@ myPromise
 .catch(error => {
     console.log(error); // If promise is rejected, log the error message
 });
+
+//Asynchronous Operation
+let fetchData = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let dataFetched = true; // Change this to false to simulate an error
+
+    if (dataFetched) {
+        resolve("Data fetched successfully!");
+    } else {
+        reject("Error: Data fetch failed!");
+}
+    }, 2000);
+});
+
+fetchData
+    .then((message) => {
+      console.log(message); // Logs after 2 seconds: 'Data fetched successfully!'
+    })
+    .catch((error) => {
+      console.log(error); // Logs if the promise is rejected
+    });
+
 
 
