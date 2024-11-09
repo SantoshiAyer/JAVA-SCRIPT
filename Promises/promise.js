55bf4ce0354f0209d142f2794f40f54d46d346c9
santoshi ayer <santoshiayer12@gmail.com>
2024-11-09 11:00:12 +0545

    commit

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,43 +1,43 @@
 let myPromise = new Promise((resolve, reject) => {
-    // Simulate an asynchronous operation
-    let success = true; // Change this to false to test rejection
+// Simulate an asynchronous operation
+let success = true; // Change this to false to test rejection
 
-    if (success) {
-    resolve('Operation successful!');
-    } else {
-    reject('Operation failed!');
-    }
+if (success) {
+resolve('Operation successful!');
+} else {
+reject('Operation failed!');
+}
 });
 
-  //handling promises results
+//handling promises results
 myPromise
 .then(result => {
-    console.log(result); // Logs: 'Operation successful!'
+console.log(result); // Logs: 'Operation successful!'
 })
 .catch(error => {
-    console.log(error); // If promise is rejected, log the error message
+console.log(error); // If promise is rejected, log the error message
 });
 
 //Asynchronous Operation
 let fetchData = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let dataFetched = true; // Change this to false to simulate an error
+setTimeout(() => {
+let dataFetched = true; // Change this to false to simulate an error
 
-    if (dataFetched) {
-        resolve("Data fetched successfully!");
-    } else {
-        reject("Error: Data fetch failed!");
+if (dataFetched) {
+resolve("Data fetched successfully!");
+} else {
+reject("Error: Data fetch failed!");
 }
-    }, 2000);
+}, 2000);
 });
 
 fetchData
-    .then((message) => {
-      console.log(message); // Logs after 2 seconds: 'Data fetched successfully!'
-    })
-    .catch((error) => {
-      console.log(error); // Logs if the promise is rejected
-    });
+.then((message) => {
+console.log(message); // Logs after 2 seconds: 'Data fetched successfully!'
+})
+.catch((error) => {
+console.log(error); // Logs if the promise is rejected
+});
 
 
 
