ef900888889e4415fb056c689cb7c60a97d78ac8
santoshi ayer <santoshiayer12@gmail.com>
2024-11-14 18:54:31 +0545

    commit

--- Promises/pro.js
+++ Promises/pro.js
@@ -1,2 +1,13 @@
 //promise chain
-console.log("getting data1..")
+console.log("getting data1..");
+gateData(1);
+.then((res)=>{
+    console.log("getting data2....");
+    return getData(2);
+})
+.then((res)=>{
+    return getData(3);
+})
+.then((res)=>{
+    console.log(res);
+});
