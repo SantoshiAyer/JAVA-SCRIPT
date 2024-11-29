755a8d158b1d6e6e613285690106155bb366581e
santoshi ayer <santoshiayer12@gmail.com>
2024-11-29 07:39:36 +0545

    commited

--- DOM/clock.js
+++ DOM/clock.js
@@ -2,5 +2,5 @@ const clock = document.getElementById('clock'); // Get the clock element
 
 setInterval(function () {
     let date = new Date(); // Get the current date and time
-    clock.innerHTML = date.toLocaleTimeString(); // Update the clock with the current time
+    clock.innerHTML = date.toLocaleTimeString(); // Update the clock with the current time..
 }, 1000); // Update every second (1000 milliseconds)
